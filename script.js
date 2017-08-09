8efea57718d170cfe33b0438b2e89b82d61376df
Hack Reactor Student <student@hackreactor.com>
2017-08-09 12:31:25 -0500

    wow

--- script.js
+++ script.js
@@ -1,5 +1,72 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+//
 $(document).ready(function(){
 // your code goes below
+
+console.log("page is loaded");
+
+$("#word-1").click(function(){
+ $("#word-1").hide(1000);
+})
+
+$("#word-2").click(function(){
+ $("#word-2").hide(1000);
+})
+
+$("#word-3").click(function(){
+ $("#word-3").hide(1000);
+})
+
+$("#word-4").click(function(){
+ $("#word-4").hide(1000);
+})
+
+$("#word-5").click(function(){
+ $("#word-5").hide(1000);
+})
+
+$("#word-6").click(function(){
+ $("#word-6").hide(1000);
+})
+
+$("#word-7").click(function(){
+ $("#word-7").hide(1000);
+})
+
+$("#word-8").click(function(){
+ $("#word-8").hide(1000);
+})
+
+$("#word-9").click(function(){
+ $("#word-9").hide(1000);
+})
+
+$("#word-10").click(function(){
+ $("#word-10").hide(1000);
+})
+
+$("#word-11").click(function(){
+ $("#word-11").hide(1000);
+})
+
+$("#word-12").click(function(){
+ $("#word-12").hide(1000);
+})
+
+$("#word-13").click(function(){
+ $("#word-13").hide(1000);
+})
+
+$("#word-14").click(function(){
+ $("#word-14").hide(1000);
+})
+
+$("#word-15").click(function(){
+ $("#word-15").hide(1000);
+})
+
+$("#word-16").click(function(){
+ $("#word-16").hide(1000);
+})
+
 })
